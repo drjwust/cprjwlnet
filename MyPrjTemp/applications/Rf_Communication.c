767901444f153312aa08b8797c743386893f954b
drjwhut <670065868@qq.com>
2014-04-11 20:30:55 +0800

    NULL

--- MyPrjTemp/applications/Rf_Communication.c
+++ MyPrjTemp/applications/Rf_Communication.c
@@ -14,39 +14,23 @@ void rf_thread_entry(void *para)
 {
 	uint8_t leng = 0;
 	uint8_t tf = 0;
-	uint8_t TxBuf[8] =
-	{ 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };	// 8字节, 如果需要更长的数据包,请正确设置
-	uint8_t RxBuf[8] =	{ 0 };
-
+	RF_DATA * pRfData;
 	rf_init();
 	halSpiWriteBurstReg(CCxxx0_PATABLE, PaTabel, 8);
 	halSpiReadStatus(CCxxx0_VERSION);
-#if 0
+
 	halRfReceiveEnable();
 	while (1)
 	{
-		leng = 8;
-		halRfReceivePacket(RxBuf, leng);	// Transmit Tx buffer data
-		if (RxBuf[0] == 0x01)
+		pRfData = rt_malloc(sizeof(RF_DATA));
+		halRfReceivePacket((uint8_t* )pRfData, sizeof(RF_DATA));	// Transmit Tx buffer data
+		if (pRfData->dst_addr == 20)
 		{
 			rt_thread_delay(600);
 			rt_hw_led_off(2);
 			rt_thread_delay(600);
 			rt_hw_led_on(2);
-			RxBuf[0] = 0;
+			pRfData->dst_addr =0;
 		}
 	}
-#else
-	halRfTransmitEnable();
-//	halRfReceiveEnable();
-	while (1)
-	{
-		leng=8;
-		halRfSendPacket(TxBuf,leng);	// Transmit Tx buffer data
-		rt_thread_delay(300);
-		rt_hw_led_on(2);
-		rt_thread_delay(300);
-		rt_hw_led_off(2);
-	}
-#endif
 }
