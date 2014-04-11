2a6cf727f2e42c6425bca2485c219b7883b7815b
drjwhut <670065868@qq.com>
2014-04-11 20:17:43 +0800

    注释掉了rtconfig.h中的RT_USING_MINILIBC

--- MyPrjTemp/applications/Rf_Communication.c
+++ MyPrjTemp/applications/Rf_Communication.c
@@ -14,9 +14,7 @@ void rf_thread_entry(void *para)
 {
 	uint8_t leng = 0;
 	uint8_t tf = 0;
-	uint8_t TxBuf[8] =
-	{ 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };	// 8字节, 如果需要更长的数据包,请正确设置
-	uint8_t RxBuf[8] =	{ 0 };
+	RF_DATA* pRfData;
 
 	rf_init();
 	halSpiWriteBurstReg(CCxxx0_PATABLE, PaTabel, 8);
@@ -38,11 +36,15 @@ void rf_thread_entry(void *para)
 	}
 #else
 	halRfTransmitEnable();
-//	halRfReceiveEnable();
+	pRfData = rt_malloc(sizeof(RF_DATA));
+	pRfData->dst_addr = 12;
+	pRfData->src_addr = 0;
+	pRfData->temperature = 35;
+	pRfData->state = 0;
 	while (1)
 	{
 		leng=8;
-		halRfSendPacket(TxBuf,leng);	// Transmit Tx buffer data
+		halRfSendPacket(&pRfData,sizeof(RF_DATA));	// Transmit Tx buffer data
 		rt_thread_delay(300);
 		rt_hw_led_on(2);
 		rt_thread_delay(300);
