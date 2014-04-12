0ddf8578df63ef191019b52ba83ad55b17d80fce
drjwhut <670065868@qq.com>
2014-04-12 18:38:59 +0800

    解决了RF线程死机的问题

--- MyPrjTemp/applications/Rf_Communication.c
+++ MyPrjTemp/applications/Rf_Communication.c
@@ -35,41 +35,46 @@ static void rf_thread_entry(void *para)
 		halRfSetRxMode();
 		rt_sem_take(&rf_sem, RT_WAITING_FOREVER);	//等待数据接收完成
 		halRfReceivePacket((uint8_t*) pRfData, &leng);	//接收到的数据放入pRfData中
-		node = pRfData->src_addr;
-		NodeList->node[node].temperature = pRfData->temperature & 0X3F;	//低10位数据为温度
-		NodeList->node[node].state = pRfData->state;
-		now = time(RT_NULL), tmp = localtime(&now);
-		rt_sprintf(filedata, "%d\t%d\t%d\t%d\t%d\t\t%d年%d月%d日\t%d:%d\n", node,
-				NodeList->node[node].temperature, NodeList->node[node].state,
-				NodeList->node[node].relative_alarm_value,
-				NodeList->node[node].abs_alram_value, tmp->tm_year + 1900,
-				tmp->tm_mon + 1, tmp->tm_mday, tmp->tm_hour, tmp->tm_min);
-		leng = strlen(filedata);
-		fd = open("/data.dat", O_RDWR, 0);
-		lseek(fd, 0, DFS_SEEK_END);
-		write(fd,filedata,leng);
-		close(fd);
-		if (pRfData->state != 0)
+		if (leng != 0)
 		{
-			NodeList->error_node[NodeList->error_num] = &NodeList->node[node];
-			fd = open("/alarm.dat", O_RDWR, 0);
+			node = pRfData->src_addr;
+			NodeList->node[node].temperature = pRfData->temperature & 0X3F;	//低10位数据为温度
+			NodeList->node[node].state = pRfData->state;
+			now = time(RT_NULL), tmp = localtime(&now);
+			rt_sprintf(filedata, "%d\t%d\t%d\t%d\t%d\t\t%d年%d月%d日\t%d:%d\n",
+					node, NodeList->node[node].temperature,
+					NodeList->node[node].state,
+					NodeList->node[node].relative_alarm_value,
+					NodeList->node[node].abs_alram_value, tmp->tm_year + 1900,
+					tmp->tm_mon + 1, tmp->tm_mday, tmp->tm_hour, tmp->tm_min);
+			leng = strlen(filedata);
+			fd = open("/data.dat", O_RDWR, 0);
 			lseek(fd, 0, DFS_SEEK_END);
-			write(fd,filedata,leng);
+			write(fd, filedata, leng);
 			close(fd);
-		}
-		if (pRfData->dst_addr == 12)
-		{
+			if (pRfData->state != 0)
+			{
+				NodeList->error_node[NodeList->error_num] =
+						&NodeList->node[node];
+				fd = open("/alarm.dat", O_RDWR, 0);
+				lseek(fd, 0, DFS_SEEK_END);
+				write(fd, filedata, leng);
+				close(fd);
+			}
+			if (pRfData->dst_addr == 1)
+			{
 //			rt_kprintf("received data\n");
-			if (state == RESET)
-			{
-				state = SET;
+				if (state == RESET)
+				{
+					state = SET;
+				}
+				else
+				{
+					state = RESET;
+				}
+				rt_hw_led_set_state(2, state);
+				pRfData->dst_addr = 0;
 			}
-			else
-			{
-				state = RESET;
-			}
-			rt_hw_led_set_state(2, state);
-			pRfData->dst_addr = 0;
 		}
 	}
 }
@@ -79,7 +84,8 @@ void rf_thread_init(void)
 	rt_thread_t init_thread;
 
 	rt_sem_init(&rf_sem, "rf", 0, RT_IPC_FLAG_FIFO);
-	init_thread = rt_thread_create("rf", rf_thread_entry, RT_NULL, 2048, 10, 20);
+	init_thread = rt_thread_create("rf", rf_thread_entry, RT_NULL, 2048, 10,
+			20);
 	if (init_thread != RT_NULL)
 	{
 		rt_thread_startup(init_thread);
