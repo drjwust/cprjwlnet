4329f14e7a91f43953f548564e6263a7a7fb9691
drjwhut <670065868@qq.com>
2014-04-14 10:11:22 +0800

    测试CS

--- MyPrjTemp/applications/Rf_Communication.c
+++ MyPrjTemp/applications/Rf_Communication.c
@@ -12,15 +12,16 @@ uint8_t PaTabel[8] =
 
 void rf_thread_entry(void *para)
 {
-	uint8_t leng = 0;
-	uint8_t tf = 0,status;
+	uint8_t leng = 0, error;
+	uint8_t tf = 0, status[2];
 	RF_DATA* pRfData;
 
 	rf_init();
 	halSpiWriteBurstReg(CCxxx0_PATABLE, PaTabel, 8);
 	halSpiReadStatus(CCxxx0_VERSION);
 
-	halRfTransmitEnable();
+	halRfReceiveEnable();
+//	halRfTransmitEnable();
 	pRfData = rt_malloc(sizeof(RF_DATA));
 	pRfData->dst_addr = 1;
 	pRfData->src_addr = 0;
@@ -30,16 +31,41 @@ void rf_thread_entry(void *para)
 	{
 		pRfData->src_addr += 1;
 		if (pRfData->src_addr == 20)
-			pRfData->src_addr =0;
+			pRfData->src_addr = 0;
 		pRfData->temperature += 1;
 		if (pRfData->temperature == 150)
 			pRfData->temperature = 1;
-		halRfSendPacket(pRfData,sizeof(RF_DATA));	// Transmit Tx buffer data
-		rt_thread_delay(50);
+//		halRfSendPacket(pRfData,sizeof(RF_DATA));	// Transmit Tx buffer data
+		halRfReceiveEnable();
+		halSpiStrobe(CCxxx0_SRX);
+		error = rt_sem_take(&rf_sem, 15);
+		if (error == RT_EOK)
+		{
+			leng = halRfReceivePacket((uint8_t*) pRfData, status);
+			if (leng != 0)
+				rt_kprintf("接收成功(1),RSSI:%d\t", status[0]);
+			else
+			{
+				status[0] = halSpiReadStatus(CCxxx0_RSSI);
+				rt_kprintf("接收错误(2),RSSI:%d\t", status[0]);
+			}
+		}
+		else
+		{
+			status[0] = halSpiReadStatus(CCxxx0_RSSI);
+			halRfTransmitEnable();
+			rt_kprintf("接收失败(3),RSSI:%d\n", status[0]);
+			halRfSendPacket((uint8_t* )pRfData, sizeof(RF_DATA));
+		}
+//		if (leng == 0)
+//		{
+//			status[0] = halSpiReadStatus(CCxxx0_RSSI);
+//		}
+
+//		rt_thread_delay(100);
 		rt_hw_led_on(2);
-		rt_thread_delay(50);
+//		rt_thread_delay(100);
 		rt_hw_led_off(2);
-		halRfReadStatusReg(CCxxx0_RSSI);
 	}
 }
 
